dd502a48331469b17df87c229c7d70933943ddf7
Michael Wellner <michael.wellner@de.ibm.com>
2016-10-18 13:27:37 +0200

    sc.

--- application/src/main/java/mgmt/ServiceDiscoveryHeartbeat.java
+++ application/src/main/java/mgmt/ServiceDiscoveryHeartbeat.java
@@ -2,12 +2,15 @@ package mgmt;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.Optional;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+
+import org.apache.commons.lang3.tuple.Pair;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -22,21 +25,29 @@ import ws.AboutService;
 @Singleton
 public class ServiceDiscoveryHeartbeat extends BaseObject {
 
-	private static final String VCS_SERVICE_DISCOVERY = "service_discovery";
-	private static final String VCS_SERVICE_DISCOVERY_CREDENTIALS = "credentials";
-	private static final String VCS_SERVICE_DISCOVERY_AUTH_TOKEN = "auth_token";
-	private static final String VCS_SERVICE_DISCOVERY_URL = "url";
-
 	private static final String VCA_URIS = "uris";
+	private static final String VCS_SERVICE_DISCOVERY = "service_discovery";
+	private static final String VCS_SERVICE_DISCOVERY_AUTH_TOKEN = "auth_token";
+	private static final String VCS_SERVICE_DISCOVERY_CREDENTIALS = "credentials";
+
+	private static final String VCS_SERVICE_DISCOVERY_URL = "url";
 
 	@EJB
 	private AboutService about;
 
 	private Register registerRequest;
 
+	private Optional<Pair<String, String>> serviceDiscoveryCredentials;
+
 	@PostConstruct
 	public void before() {
 		this.registerRequest = registerService$createRequest();
+		this.serviceDiscoveryCredentials = getServiceDiscoveryCredentials();
+		registerService();
+	}
+
+	@Schedule(second = "0", minute = "*/1", hour = "*", persistent = false)
+	public void heartbeat() {
 		registerService();
 	}
 
@@ -69,23 +80,17 @@ public class ServiceDiscoveryHeartbeat extends BaseObject {
 		return CONFIG.getString("app.name") + ".mybluemix.net";
 	}
 
-	@Schedule(second = "0", minute = "*/1", hour = "*", persistent = false)
-	public void heartbeat() {
-		registerService();
-	}
-
-	private void registerService() {
+	private Optional<Pair<String, String>> getServiceDiscoveryCredentials() {
 		ObjectMapper m = new ObjectMapper();
-		JsonNode vcapServices;
+		JsonNode vcapServices = null;
 
 		try {
 			vcapServices = m.readTree(CONFIG.getString("vcap.services"));
 		} catch (IOException e) {
 			LOG.warn("Unable to parse VCAP_SERVICES from configuration variable `vcap.services`.");
-			return;
 		}
 
-		if (vcapServices.get(VCS_SERVICE_DISCOVERY) != null) {
+		if (vcapServices != null && vcapServices.get(VCS_SERVICE_DISCOVERY) != null) {
 			try {
 				Iterator<JsonNode> elements = vcapServices.get(VCS_SERVICE_DISCOVERY).elements();
 
@@ -94,23 +99,34 @@ public class ServiceDiscoveryHeartbeat extends BaseObject {
 					JsonNode credentials = serviceDiscovery.get(VCS_SERVICE_DISCOVERY_CREDENTIALS);
 					String authToken = credentials.get(VCS_SERVICE_DISCOVERY_AUTH_TOKEN).asText();
 					String url = credentials.get(VCS_SERVICE_DISCOVERY_URL).asText();
-
-					HttpResponse<String> response = Unirest //
-							.post(url + "/api/v1/instances") //
-							.header("Content-Type", "application/json") //
-							.header("Accept", "application/json") //
-							.header("Authorization", "Bearer " + authToken) //
-							.body(registerRequest.toString()) //
-							.asString();
-
-					LOG.debug(response.getBody());
+					return Optional.of(Pair.of(authToken, url));
 				}
 			} catch (Exception e) {
 				LOG.warn("Unable to register at service discovery", e.getMessage());
-				e.printStackTrace();
 			}
 		} else {
 			LOG.warn("No VCAP_SERVICES defined in variable `vcap.services`.");
+		}
+
+		return Optional.empty();
+	}
+
+	private void registerService() {
+		try {
+			if (serviceDiscoveryCredentials.isPresent()) {
+				HttpResponse<String> response = Unirest //
+						.post(serviceDiscoveryCredentials.get().getRight() + "/api/v1/instances") //
+						.header("Content-Type", "application/json") //
+						.header("Accept", "application/json") //
+						.header("Authorization", "Bearer " + serviceDiscoveryCredentials.get().getLeft()) //
+						.body(registerRequest.toString()) //
+						.asString();
+
+				LOG.debug(response.getBody());
+			}
+		} catch (Exception e) {
+			LOG.warn("Unable to register at service discovery", e.getMessage());
+			e.printStackTrace();
 		}
 	}
 
